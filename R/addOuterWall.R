d70780492af8e99f035e91a5ab9e8782cae5ce83
joost0252 <33458844+joost0252@users.noreply.github.com>
2023-08-28 11:27:21 +0200

    Removal of not used function file

--- R/addOuterWall.R
+++
@@ -1,5 +0,0 @@
-add_outer_wall <- function(maze_df, width, height) {
-  maze_df$cell_color[maze_df$x == 1 | maze_df$x == width] <- "black"
-  maze_df$cell_color[maze_df$y == 1 | maze_df$y == height] <- "black"
-  return(maze_df)
-}
